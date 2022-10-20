eb9719d901ee9b6bc5a23e48d557975c70defb27
GRieZzLEO <chamodhleo1@gmail.com>
2022-10-20 14:57:11 +0530

    Added stuff

--- app/src/main/java/com/example/menntuneducationalapplication/MainActivity.java
+++ app/src/main/java/com/example/menntuneducationalapplication/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.menntuneducationalapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Button forumB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //forumB=findViewById(R.id.forumBtn);
+    }
+
+    public void toForum(View view){
+        Intent X=new Intent(this,Forums.class);
+        startActivity(X);
     }
 }

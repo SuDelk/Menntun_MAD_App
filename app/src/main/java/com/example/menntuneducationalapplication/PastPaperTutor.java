a7aa2034282eeab6bdd981e88cc39755f3807c1c
IT21024740 <it21024740@my.sliit.lk>
2022-10-23 20:42:31 +0530

    Past Papers Create Form

--- app/src/main/java/com/example/menntuneducationalapplication/PastPaperTutor.java
+++ app/src/main/java/com/example/menntuneducationalapplication/PastPaperTutor.java
@@ -5,17 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class PastPaperTutor extends AppCompatActivity {
+    Button addNewPP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_past_paper_tutor);}
+        setContentView(R.layout.activity_past_paper_tutor);
 
+        addNewPP = findViewById(R.id.addpp);
+        addNewPP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PastPaperTutor.this, AddPastPapers.class);
+                startActivity(intent);
+            }
 
-        public void onClick(View view){
-        Intent p =  new Intent(PastPaperTutor.this, AddPastPapers.class);
-            startActivity(p);
-        }
+        });
     }
+}
+
+

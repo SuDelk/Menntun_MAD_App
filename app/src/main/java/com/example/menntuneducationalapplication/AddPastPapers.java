540a10350fbbffe85b21912021233d2bdda1aa91
IT21024740 <it21024740@my.sliit.lk>
2022-10-24 22:41:21 +0530

    fix the bug

--- app/src/main/java/com/example/menntuneducationalapplication/AddPastPapers.java
+++ app/src/main/java/com/example/menntuneducationalapplication/AddPastPapers.java
@@ -16,17 +16,15 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnSuccessListener;
+
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
+
 
 public class AddPastPapers extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
+    //variable
     EditText subject;
     Spinner spinner2;
     String grade[];
@@ -37,12 +35,13 @@ public class AddPastPapers extends AppCompatActivity implements AdapterView.OnIt
     EditText pdfname;
 
 
-    StorageReference storageReference;
+
     DatabaseReference PastPaperDbRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_past_papers);
+
         spinner1 = findViewById(R.id.yearSpinner);
         subject = findViewById(R.id.subjectPP);
         spinner2 = findViewById(R.id.gradepp);
@@ -50,14 +49,9 @@ public class AddPastPapers extends AppCompatActivity implements AdapterView.OnIt
         submit = findViewById(R.id.submitPP);
         pdfname=findViewById(R.id.UploadPPtext);
 
-        storageReference =FirebaseStorage.getInstance().getReference();
+
         PastPaperDbRef = FirebaseDatabase.getInstance().getReference().child("PastPaper");
-        upload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                selectFiles();
-            }
-        });
+
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,57 +74,8 @@ public class AddPastPapers extends AppCompatActivity implements AdapterView.OnIt
 
     }
 
-    private void selectFiles() {
-        Intent intent=new Intent();
-        intent.setType("application/pdf");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent,"select pdf file"),1);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode==1 && resultCode==RESULT_OK && data!=null && data.getData()!=null){
-            UploadFiles(data.getData());
-        }
 
 
-
-    }
-
-    private void UploadFiles(Uri data) {
-        final ProgressDialog progressDialog = new ProgressDialog(this);
-        progressDialog.setTitle("Uploading...");
-        progressDialog.show();
-
-        StorageReference reference = storageReference.child("Uploads/" + System.currentTimeMillis() + "pdf");
-        reference.putFile(data)
-                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-
-
-                        Task<Uri> uriTask=taskSnapshot.getStorage().getDownloadUrl();
-                        while(!uriTask.isComplete());
-                        Uri url=uriTask.getResult();
-
-                        pdfClass pdfClass = new pdfClass(pdfname.getText().toString(),url.toString());
-                        PastPaperDbRef.child(PastPaperDbRef.push().getKey()).setValue(pdfClass);
-
-                        Toast.makeText(AddPastPapers.this,"File Upload",Toast.LENGTH_SHORT).show();
-
-                        progressDialog.dismiss();
-
-                    }
-                }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
-                        double progress=(100.0* snapshot.getBytesTransferred())/snapshot.getTotalByteCount();
-                        progressDialog.setMessage("Uploaded "+(int)progress+"%");
-                    }
-                });
-    }
 
     private void insertPastPaperData(){
         String Subject = subject.getText().toString();
